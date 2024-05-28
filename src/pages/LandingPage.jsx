74973c62e3e8ef722a958b3d1cc47ccd873cfdb4
kalikokalikova <calico.seders@gmail.com>
2024-05-27 21:51:09 -0700

    add landing page image

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import daylightLanding from '../assets/daylightLanding.png';
+
+const landingPageStyles = {
+    container: {
+      backgroundImage: `url(${daylightLanding})`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      height: '100vh',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'white',
+      textAlign: 'center'
+    },
+    heading: {
+      fontSize: '3em',
+      margin: '0'
+    }
+  };
 
 function LandingPage() {
   return (
+    <div style={landingPageStyles.container}>
     <h1>LandingPage</h1>
+    </div>
+
   )
 }
 
