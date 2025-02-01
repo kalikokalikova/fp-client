c14fdae42f79cf171684f9031d81294b4f9c9db9
kalikokalikova <calico.seders@gmail.com>
2025-01-31 18:57:14 -0800

    install react query, basics in place for show event page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import NavBar from "./components/NavBar";
 import "./App.css";
 import "normalize.css";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
+    <QueryClientProvider client={queryClient}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <NavBar />
         <Outlet />
       </LocalizationProvider>
+      </QueryClientProvider>
     </>
   );
 }
--- src/components/events/Event.jsx
+++ src/components/events/Event.jsx
@@ -1,54 +1,54 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import api from "../../api";
 import { Box, Button, Tab, Tabs, Typography } from "@mui/material";
 import QRCode from "react-qr-code";
 
 export default function Event() {
-  const [event, setEvent] = useState(null);
   const params = useParams();
   const [value, setValue] = useState(0);
 
-  useEffect(() => {
-    setEvent({
-      allowQA: true,
-      description: "A really good description. Very very nice indeed.",
-      endDateTime: null,
-      endTime: "2024-11-16T12:20:00.000Z",
-      hostName: "Calico Seders",
-      isShareable: true,
-      location: {
-        locationName: "MiniGolf Course",
-        addressLine1: "123 Main Loop",
-        addressLine2: "Seattle, WA 98103, United States of America",
-        city: "Seattle",
-        country: "United States",
-        locationText:
-          "123 Main Loop Seattle, WA 98103, United States of America",
-        placeId:
-          "51e7ea7d995e965ec0593ea2f8e758d54740f00102f901027a350300000000c00203",
-        postcode: "98103",
-        state: "Washington",
-      },
-      startDateTime: "2024-11-15T21:34:37.255Z",
-      title: "Your mom",
-    });
-  }, []);
-
-  // fetch event by event id
-  useEffect(() => {
-    const fetchData = async () => {
+    // fetch event by event id
+    const fetchEvent = async () => {
+      return ({
+        allowQA: true,
+        description: "A really good description. Very very nice indeed.",
+        endDateTime: null,
+        endTime: "2024-11-16T12:20:00.000Z",
+        hostName: "Calico Seders",
+        isShareable: true,
+        location: {
+          locationName: "MiniGolf Course",
+          addressLine1: "123 Main Loop",
+          addressLine2: "Seattle, WA 98103, United States of America",
+          city: "Seattle",
+          country: "United States",
+          locationText:
+            "123 Main Loop Seattle, WA 98103, United States of America",
+          placeId:
+            "51e7ea7d995e965ec0593ea2f8e758d54740f00102f901027a350300000000c00203",
+          postcode: "98103",
+          state: "Washington",
+        },
+        startDateTime: "2024-11-15T21:34:37.255Z",
+        title: "Your mom",
+      })
       try {
         let response = await api.get(`/event/${params.eventId}`);
         console.log("Here's the response: ", response);
         // setEvent(response.data);
-
       } catch (error) {
         console.log("Here's the error: ", error);
       }
     };
-    fetchData();
-  }, []);
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["event", 1],
+    queryFn: fetchEvent,
+  });
+
+
 
   const handleTabChange = (event, newValue) => {
     setValue(newValue);
@@ -79,17 +79,22 @@ export default function Event() {
 
   return (
     <>
-      {event ? (
+      {data ? (
         <Box>
           <Box sx={{ display: "flex", justifyContent: "space-around" }}>
             <Box sx={{ display: "flex", flexDirection: "column" }}>
-              <Box>{event.title}</Box>
+              <Box>{data.title}</Box>
               <Box>
                 <Button>save</Button>
                 <Button>Share</Button>
               </Box>
             </Box>
-            <Box><QRCode value="https://www.geeksforgeeks.org/how-to-make-a-qr-code-generator-using-qrcode-js/"  style={{ height: "auto", maxWidth: "50%", width: "50%" }} /></Box>
+            <Box>
+              <QRCode
+                value="https://www.geeksforgeeks.org/how-to-make-a-qr-code-generator-using-qrcode-js/"
+                style={{ height: "auto", maxWidth: "50%", width: "50%" }}
+              />
+            </Box>
           </Box>
           <Box sx={{ width: "100%" }}>
             <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
@@ -106,13 +111,13 @@ export default function Event() {
             <EventTabPanel value={value} index={0}>
               <Box>
                 <Typography>When</Typography>
-                <Typography>{event.startDateTime}</Typography>
+                <Typography>{data.startDateTime}</Typography>
                 <Typography>icon Add to calendar link</Typography>
                 <Typography>Where</Typography>
                 <Typography>location breakdown goes here</Typography>
                 <Typography>What</Typography>
-                <Typography>{event.description}</Typography>
-                <Typography>Organized by {event.hostName}</Typography>
+                <Typography>{data.description}</Typography>
+                <Typography>Organized by {data.hostName}</Typography>
                 <Typography></Typography>
               </Box>
             </EventTabPanel>
